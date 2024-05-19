8d6b395520678536c8a937431bf7dfcc0cf72d38
Evan Kennedy <evanckennedy@protonmail.com>
2024-05-19 14:55:54 -0500

    fetch movie from slug

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios'; 
 
 function MovieDetails() {
   let { slug } = useParams();
+  const [movie, setMovie] = useState(null);
 
-/*   Use the slug to fetch or display the details of the movie
+  const URL = 'https://api.andrespecht.dev/movies/';
+
   useEffect(() => {
-    fetchMovieDetails(slug);
-  }, [slug]); */
+    async function getMovie() {
+      try {
+        const response = (await axios.get(URL)).data.response;
+        const movieFound = response.find(movie => movie.slug === slug)
+        setMovie(movieFound);
+        console.log(movie)
+      } catch (error) {
+        console.error('Failed to fetch movie:', error);
+      }
+    }
+
+    getMovie()
+  }, [slug])
 
   return (
     <div>MovieDetails</div>
