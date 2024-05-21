35a517f1351c09994e2d1ffd3266b2c0e14731d9
Evan Kennedy <evanckennedy@protonmail.com>
2024-05-20 23:23:41 -0500

    fetch and display movie details from slug

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 function MovieDetails() {
   let { slug } = useParams();
   const [movie, setMovie] = useState(null);
+  /* const [loading, setLoading] = useState(true); */
 
   const URL = 'https://api.andrespecht.dev/movies/';
 
@@ -14,17 +15,43 @@ function MovieDetails() {
         const response = (await axios.get(URL)).data.response;
         const movieFound = response.find(movie => movie.slug === slug)
         setMovie(movieFound);
-        console.log(movie)
       } catch (error) {
         console.error('Failed to fetch movie:', error);
       }
     }
 
-    getMovie()
+    getMovie();
   }, [slug])
 
   return (
-    <div>MovieDetails</div>
+    /*     
+      Using `movie && (...)` to conditionally render movie details.
+      This prevents a TypeError when `movie` is initially `null` during data fetching.
+      It ensures rendering only occurs after `movie` state is updated with fetched data.
+    */
+    movie && (
+    <>
+      <div className="poster-wrapper">
+        <figure>
+          <img src={movie.poster} alt={movie.title}/>
+        </figure>
+      </div>
+      <div className="information-container">
+        <div className="information">
+          <h2>{movie.title}</h2>
+          <p className="release-duration">
+            <span>{movie.year}</span> | <span>{movie.runningTime}</span>
+          </p>
+          <p className="description">{movie.description}</p>
+          <p className="genres flex">
+            {movie.genre.map((genre, index) => (
+              <span key={index}>{genre}</span>
+            ))}
+          </p>
+        </div>
+      </div>
+    </>
+    )  
   )
 }
 
