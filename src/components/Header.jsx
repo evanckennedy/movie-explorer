29c72e8681d6c2e8d7800e607a973777f02f7535
Evan Kennedy <evanckennedy@protonmail.com>
2024-05-26 23:23:14 -0500

    make header a link to the home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
   return (
     <header className='container'>
-      <h1>Movie Explorer</h1>
-      <p className="sub-header">Discover Your Next Favourite Film</p>
+      <Link to={'/'}>
+        <h1>Movie Explorer</h1>
+        <p className="sub-header">Discover Your Next Favourite Film</p>
+      </Link>
     </header>    
   )
 }
